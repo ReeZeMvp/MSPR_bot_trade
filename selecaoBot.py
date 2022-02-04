cd7211e68283abd7248298d3eb33684fd81757b8
ReeZeMvp <reezemvp@gmail.com>
2022-02-04 16:32:40 +0100

    Test 1.0

---
+++ selecaoBot.py
@@ -0,0 +1,19 @@
+class selecaoBot():
+
+    def __init__(self):
+        self.money = 100000
+        self.nom = "mdr"
+
+    def process_candle(self, candle_msg:str):
+        """This function is called when a new candle_msg is received.
+            Candle message is a string of the form:
+            'symbol_key' : {'c': [174.3], 'h': [174.3], 'l': [174.19], 'o': [174.19], 's': 'ok', 't': [1643670000], 'v': [1888]}
+
+            Note that there are list, so you can have multiple candles in one message.
+        """
+
+    def gains(self):
+        return self.money
+    
+    def Nom(self):
+        return self.nom 
