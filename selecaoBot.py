b68db5607e1a88ed07c5184d8db66011dcff72e3
remi_ccel <rcancel2003@gmail.com>
2022-02-14 16:54:13 +0100

    0.1.1

--- selecaoBot.py
+++ selecaoBot.py
@@ -1,9 +1,11 @@
+import json 
 class selecaoBot():
 
-    def __init__(self):
-        self.money = 100000
-        self.nom = "mdr"
-        self.truc ="rems"
+    def __init__(self,client):
+        self.client = client  
+        self.total_AAPL = 0
+        self.count_AAPL = 0
+        self.previous_price = 0 
 
     def process_candle(self, candle_msg:str):
         """This function is called when a new candle_msg is received.
@@ -12,12 +14,20 @@ class selecaoBot():
 
             Note that there are list, so you can have multiple candles in one message.
         """
+        candle_dict = json.loads(candle_msg)                           #transforme le fichier json en dictionnaire 
+        for k, v in candle_dict.items():
+            if 'AAPL' == k :
+                self.update_aapl_mean(v)
 
-    def gains(self):
-        return self.money
-    
-    def Nom(self):
-        return self.nom 
+                print(f"Moyenne :{self.total_AAPL/self.count_AAPL}")
+                self.sell_if_needed(k, v)
 
-    def machin (self):
-        return self.truc 
+    def update_aapl_mean(self, v):                                       #remanier ctrl shift r, permet de créer une méthode sans la réécrire, plus clair
+        self.count_AAPL += 1
+        self.total_AAPL += v['c']
+
+    def sell_if_needed(self, k, v):
+        if v['c'] < self.previous_price  : 
+            self.client.sell(k,1)
+        elif self.client.money > v['c']: 
+            self.client.buy(k,1)
