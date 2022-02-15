5ff77ad6696a6a69b5640f83a23d50c0f5530695
remi_ccel <rcancel2003@gmail.com>
2022-02-15 10:59:28 +0100

    première solution

--- selecaoBot.py
+++ selecaoBot.py
@@ -16,11 +16,22 @@ class selecaoBot():
         """
         candle_dict = json.loads(candle_msg)                           #transforme le fichier json en dictionnaire 
         for k, v in candle_dict.items():
+            bon_prix = self.client.money * 0.01
             if 'AAPL' == k :
                 self.update_aapl_mean(v)
 
-                print(f"Moyenne :{self.total_AAPL/self.count_AAPL}")
-                self.sell_if_needed(k, v)
+                #print(f"Moyenne :{self.total_AAPL/self.count_AAPL}")
+                #self.sell_if_needed(k, v)
+            self.chute_prix(k, v, bon_prix)
+            self.previous_price = v['c']
+
+    def chute_prix(self, k, v, bon_prix):
+        """permet d'acheter une action quand son prix chute, et qu'elle avait de la valeur avant. 
+        l'action reprendra de la valeur et nos actions avec"""
+        prix_interessant = 0                                                 #donner une valeur directement
+        if v['c'] < bon_prix and self.previous_price > v['c'] :
+            self.client.buy(k,2)
+
 
     def update_aapl_mean(self, v):                                       #remanier ctrl shift r, permet de créer une méthode sans la réécrire, plus clair
         self.count_AAPL += 1
